568d29e948025f5064b3172f15797861ba2a2fdf
Forklein <giuseppepisani2@gmail.com>
2021-09-21 16:36:13 +0200

    Add input for bad word

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css/style_php.css">
     <title>Php Badwords</title>
 </head>
 <body>
@@ -11,20 +12,26 @@
 <?php 
 $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, ad provident recusandae corporis explicabo eius libero labore tempore nostrum soluta voluptas voluptatibus. Debitis corporis culpa repellendus et expedita at quas.';
 $bad_word = $_GET ['badword'];
+$edit_text = str_replace($bad_word, '***', $text);
 ?>
 
+<div class="container">
+    <div>
+        <h1>Testo Iniziale</h1>
+        <?php
+        echo "$text <br/>";
+        echo strlen($text) . " Caratteri";
+        ?>
+    </div>
+    <div>
+        <h1>Testo Modificato</h1>
+        <?php
+        echo "$edit_text <br/>";
+        echo strlen($edit_text)  . " Caratteri";
+        ?>
+    </div>
+</div>
 
-<?php
-echo "$text <br/>";
-echo strlen($text);
-echo str_replace($bad_word, '***', $text);
-?>
-
-<!-- /* Descrizione:
-Creare una variabile con un paragrafo di testo a vostra scelta.
-Stampare a schermo il paragrafo e la sua lunghezza.
-Una parola da censurare viene passata dall'utente tramite parametro GET.
-Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. */ -->
     
 </body>
 </html>
