70e013a7f1ba356f996ecdfe12b45177e4089b4a
Forklein <giuseppepisani2@gmail.com>
2021-09-21 14:45:38 +0200

    Add variable with get and str replace method

--- index.php
+++ index.php
@@ -8,7 +8,23 @@
 </head>
 <body>
 
-<?php echo 'Ciao'?>
+<?php 
+$text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, ad provident recusandae corporis explicabo eius libero labore tempore nostrum soluta voluptas voluptatibus. Debitis corporis culpa repellendus et expedita at quas.';
+$bad_word = $_GET ['badword'];
+?>
+
+
+<?php
+echo "$text <br/>";
+echo strlen($text);
+echo str_replace($bad_word, '***', $text);
+?>
+
+<!-- /* Descrizione:
+Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. */ -->
     
 </body>
 </html>
